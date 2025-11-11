51df53ee3fabb906c9f60bdda48cff8b4a88d571
VADER Orin <vader.v2.1317@gmail.com>
2025-11-11 14:22:07 -0500

    Talker publish tests for both gripper and cutter

--- scripts/dynamixel_talker_test.py
+++ scripts/dynamixel_talker_test.py
@@ -7,9 +7,9 @@ import random
 targets = [0, 20, 40, 60, 80, 100, 80, 60, 40, 20]
 def main():
     rospy.init_node('dynamixel_talker_test', anonymous=True)
-    # pub = rospy.Publisher('cutter_command', CutterCommand, queue_size=10)
-    pub = rospy.Publisher('gripper_command', GripperCommand, queue_size=10)
-    rate = rospy.Rate(0.2)  
+    pub = rospy.Publisher('cutter_command', CutterCommand, queue_size=10)
+    pub2 = rospy.Publisher('gripper_command', GripperCommand, queue_size=10)
+    rate = rospy.Rate(0.5)  
 
     curr_target_idx = 0
     while not rospy.is_shutdown():
@@ -18,14 +18,20 @@ def main():
         # rospy.loginfo(f"Publishing CutterCommand: {command}")
         # pub.publish(command)
         # rate.sleep()
-        # command = CutterCommand()
-        command = GripperCommand()
+        command = CutterCommand()
         command.open_pct = int(targets[curr_target_idx])
+        rospy.loginfo(f"Publishing CutterCommand: {command}")
+        pub.publish(command)
+
+        command2 = GripperCommand()
+        command2.open_pct = int(targets[curr_target_idx])
+        rospy.loginfo(f"Publishing GripperCommand: {command2}")
+        pub2.publish(command2)
+
+
         curr_target_idx += 1
         if curr_target_idx == len(targets):
             curr_target_idx = 0
-        rospy.loginfo(f"Publishing GripperCommand: {command}")
-        pub.publish(command)
         rate.sleep()
 
 if __name__ == '__main__':
